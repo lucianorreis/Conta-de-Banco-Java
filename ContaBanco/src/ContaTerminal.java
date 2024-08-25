b5503d3e139d9111880e6aa0a26d98093f94a83e
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-25 16:44:48 -0300

    new files to Project ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        Integer Numero = 1021;
+        String Agencia = "067-8", NomeCliente = "FULANINHO VENDAVAL";
+        Double Saldo = 237.48;
+
+        System.out.println("------- BEM VINDO! --------");
+        System.out.println("Por favor, Digite o número de sua Agência: ");
+            int numAgencia = scanner.nextInt();
+
+        if (numAgencia == Numero) {
+            System.out.println();
+        } else {
+            System.out.println("Conta não encontrada!");
+        };
+        
     }
 }
