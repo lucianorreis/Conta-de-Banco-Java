3199e46e610ace9ec9f8388704f0a44ba2ed162f
Luciano.Reis <lrr.arms233@gmail.com>
2024-08-26 19:41:00 -0300

    Update Project BancoTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -4,21 +4,25 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new 
+            Scanner(System.in).useLocale(Locale.US);
 
         Integer Numero = 1021;
         String Agencia = "067-8", NomeCliente = "FULANINHO VENDAVAL";
         Double Saldo = 237.48;
+        Integer numAgencia;
 
         System.out.println("------- BEM VINDO! --------");
-        System.out.println("Por favor, Digite o número de sua Agência: ");
-            int numAgencia = scanner.nextInt();
-
-        if (numAgencia == Numero) {
-            System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", conta " + Numero + " e seu saldo " + Saldo + " Já está disponível para saque");
-        } else {
-            System.out.println("Conta não encontrada!");
-        }
         
+        do {
+            System.out.println("Por favor, Digite o número de sua Agência: ");
+            numAgencia = scanner.nextInt();
+
+            if (numAgencia != Numero){
+                System.out.print("Erro! Agência não encontrada"); 
+            } 
+        } while (numAgencia != Numero);
+
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", conta " + Numero + " e seu saldo: " + Saldo + " Já está disponível para saque");
     }
 }
